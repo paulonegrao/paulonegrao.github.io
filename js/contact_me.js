918112336bd3c2e50fef0dceb693ae2965b07952
Paulo Negrao <paulo.negrao@ymail.com>
2016-11-01 20:48:58 -0700

    mail

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,7 +9,7 @@ $(function() {
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
-            
+
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -21,7 +21,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                // url: "././mail/contact_me.php",
+                url: "formspree.io/paulo.negrao@ymail.com",
                 type: "POST",
                 data: {
                     name: name,
